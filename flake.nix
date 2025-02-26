c663a76de28efc8ea9de0cad5728805686aaba66
ryota2357 <61523777+ryota2357@users.noreply.github.com>
2025-02-26 20:08:59 +0900

    first commit

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "TODO";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      treefmt-nix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            go
+            gopls
+            cobra-cli
+          ];
+        };
+
+        formatter = treefmt-nix.lib.mkWrapper pkgs {
+          projectRootFile = "flake.nix";
+          programs = {
+            nixfmt.enable = true;
+            gofmt.enable = true;
+            prettier = {
+              enable = true;
+              includes = [ "*.md" ];
+            };
+          };
+          settings.global.excludes = [
+            ".envrc"
+            "LICENSE"
+          ];
+        };
+      }
+    );
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ryota2357/looprun
+
+go 1.23.6
